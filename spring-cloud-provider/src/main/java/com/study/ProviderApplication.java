5ab00939e96862bd20a044f53a42f56a6ae528a1
yannengfa <940436475@qq.com>
2021-07-16 23:53:55 +0800

    将provider服务注册eureka，修改eureka上默认显示，使用actuator完善监控信息（http://localhost:8001/actuator/info访问显示info内容）

--- spring-cloud-provider/src/main/java/com/study/ProviderApplication.java
+++ spring-cloud-provider/src/main/java/com/study/ProviderApplication.java
@@ -2,9 +2,11 @@ package com.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 // 启动类
 @SpringBootApplication
+@EnableEurekaClient // 在服务启动之后自动注册到eureka
 public class ProviderApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProviderApplication.class, args);
